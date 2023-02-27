7d91b1e26ebe18b01687448eb69aa7ecdf55c5da
NicholasLaines <121262995+NicholasLaines@users.noreply.github.com>
2023-02-27 10:06:03 -0800

    More documentation

--- ionic.py
+++ ionic.py
@@ -6,7 +6,7 @@ def gen_ion_com(proccessed_formula, atomic_nums, prefixes, metalic):
     if metalic == False:
         outbound = 'amonium based ionic'
     else:
-        typei = (3,4,11,12,19,20,37,38,55,56,87,88,74)
+        typei = (3,4,11,12,19,20,37,38,55,56,87,88,74) #Group 1 metals, Group 2 metals and Tungsten
         if any(item in typei for item in atomic_nums):
             outbound = 'type i ionic compound'
         else:
