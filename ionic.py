a95adff48e054e2ab7e6dc36fb9145a0f0a74234
NicholasLaines <121262995+NicholasLaines@users.noreply.github.com>
2023-03-10 22:02:27 -0800

    Improved support for ammonium based compounds

--- ionic.py
+++ ionic.py
@@ -4,7 +4,14 @@ def gen_ion_com(proccessed_formula, atomic_nums, prefixes, metalic):
     
     
     if metalic == False:
-        outbound = 'amonium based ionic'
+        if len(proccessed_formula) == 2:
+            outbound = 'ammonium'
+        elif len(proccessed_formula) == 3:
+            atomic_nums.remove(1)
+            atomic_nums.remove(7)
+            outbound = 'ammonium ' + ele.periodic[atomic_nums[0]][3] + 'ide'
+        else:
+            outbound = 'polyatomic ammonium based ionic'
     else:
         typei = (3,4,11,12,19,20,37,38,55,56,87,88,74) #Group 1 metals, Group 2 metals and Tungsten
         if any(item in typei for item in atomic_nums):
